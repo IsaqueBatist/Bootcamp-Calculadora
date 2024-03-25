0856d2e38172a4550f28bf0bfac10ebc15bf37ca
Issaque Batista Barbosa <isaquebatista400@gmail.com>
2024-03-25 16:45:18 -0300

    closes #4 - 4 Operações

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
-function calc() {
+function soma(op) {
   var num1 = parseFloat(document.getElementById("num1").value);
   var num2 = parseFloat(document.getElementById("num2").value);
+  var valor = 0;
 
-  var soma = num1 + num2;
-  document.getElementById("output").value = soma;
+  switch (op) {
+    case '+': valor = num1 + num2; break;
+    case '-': valor = num1 - num2; break;
+    case '*': valor = num1 * num2; break;
+    case '/': valor = num1 / num2; break;
+  }
+
+  document.getElementById("output").value = valor;
 }
